4f311c674234f352788ec36f21a4f0c90783486e
Karlos Miguel Serrano <karlos_serrano2002@yahoo.com>
2026-01-14 19:52:08 +0800

    add initial implementation of enums and user type

---
+++ enums/practice.ts
@@ -0,0 +1,25 @@
+let a: number[] = [1, 2, 3];
+let b: Array<string> = ["one", "two", "three"];
+
+type TupleType = [string, number];
+let c: TupleType = ["Mark", 42];
+
+const colors: readonly string[] = ["red", "green", "blue"];
+const moreColors: readonly [number, number] = [1, 2];
+
+enum StatusEnum {
+  Active = "active",
+  Inactive = "inactive",
+  Pending = "pending",
+}
+
+type User = {
+  id: number;
+  name: string;
+  status: StatusEnum;
+};
+const Status: User = {
+  id: 1,
+  name: "John Doe",
+  status: StatusEnum.Active,
+};
