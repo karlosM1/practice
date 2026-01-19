8b90caa5186dfa44ecd1af9224e518d1489e1f0b
Karlos Miguel Serrano <karlos_serrano2002@yahoo.com>
2026-01-19 14:06:25 +0800

    refactor User and Admin classes to include phone number in constructor and improve greet method

--- class/index.ts
+++ class/index.ts
@@ -1,37 +1,52 @@
 class User {
-  name: string;
-  readonly email: string;
-  lastName?: string;
+  // lastName?: string;
+  // protected phone: number;
 
-  constructor(name: string, email: string, lastName?: string) {
-    this.name = name;
-    this.email = email;
-    this.lastName = lastName;
-  }
+  constructor(
+    public name: string,
+    public readonly email: string,
+    public phone?: number,
+    public lastName?: string,
+  ) {}
 
-  greet() {
+  public greet(): string {
     return `Hello, my name is ${this.name}`;
   }
 }
 
-const user = new User("Alice", "alice@example.com");
+const user = new User("Alice", "alice@example.com", 1234567890);
 console.log(user);
 console.log(user.greet());
 
 class Admin extends User {
-  isAdmin: boolean = true;
-  usersReporting: number;
+  public isAdmin: boolean = true;
 
   constructor(
     name: string,
     email: string,
-    usersReporting: number,
-    lastName?: string
+    phone: number,
+    public usersReporting: number,
+    lastName?: string,
   ) {
-    super(name, email, lastName);
-    this.usersReporting = usersReporting;
+    super(name, email, phone, lastName);
+  }
+
+  public printNumber() {
+    return this.phone;
+  }
+
+  public greet(): string {
+    return `Hello, I am admin ${this.name}! I am the admin`;
   }
 }
 
-const user2 = new Admin("Bob", "bob@example.com", 5);
-const admin: Admin = new Admin("Charlie", "charlie@example.com", 10);
+const user2 = new Admin("Bob", "bob@example.com", 1234567890, 5);
+const admin: Admin = new Admin(
+  "Charlie",
+  "charlie@example.com",
+  1234567890,
+  10,
+);
+
+console.log(user.greet());
+console.log(admin.greet());
